c80247c7bfcbede8c41aed51cc4e40bc61b375c1
Saurabh-Gupta890 <98148196+Saurabh-Gupta890@users.noreply.github.com>
2023-12-01 18:05:53 +0530

    create-react-app

---
+++ amazonaa/frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazona</a>
+      </header>
+      <main>
+        list of products
+      </main>
+    </div>
+  );
+}
+
+export default App;
