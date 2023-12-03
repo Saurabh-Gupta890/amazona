81759cd03ebfd6d1b62f14604f10256992a1e479
Saurabh-Gupta890 <98148196+Saurabh-Gupta890@users.noreply.github.com>
2023-12-03 16:52:31 +0530

    ui changes

--- amazonaa/frontend/src/App.js
+++ amazonaa/frontend/src/App.js
@@ -1,3 +1,5 @@
+import data from "./data";
+
 function App() {
   return (
     <div>
@@ -5,7 +7,25 @@ function App() {
         <a href="/">amazona</a>
       </header>
       <main>
-        list of products
+        <h1>Featured products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`product/${product.slug}`}>
+                <img src={product.image} alt={product.name}></img>
+              </a>
+              <div className="productInfo">
+                <a href={`product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>Rs.{product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
